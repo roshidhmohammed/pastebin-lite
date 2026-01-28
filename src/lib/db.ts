322789b63f231052490acbf2cb9f5f0eda3cbe14
roshidhmohammed <roshidhmohammed777@gmail.com>
2026-01-29 01:50:43 +0530

    "Fix MONGODB_URI type narrowing"

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,31 +1,29 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI:string | undefined = process.env.MONGODB_URI;
-
-if (!MONGODB_URI) {
+if (!process.env.MONGODB_URI) {
   throw new Error("Please define the MONGODB_URI environment variable");
 }
+const MONGODB_URI = process.env.MONGODB_URI;
 
-let cached = (global as any).mongoose;
+
+let cached = global.mongoose;
 
 if (!cached) {
-  cached = (global as any).mongoose = {
+  cached = global.mongoose = {
     conn: null,
     promise: null,
   };
 }
 
-export async function connectDB() {
-  if (cached.conn) {
-    return cached.conn;
+export async function connectDB(): Promise<mongoose.Mongoose> {
+  if (cached!.conn) {
+    return cached!.conn;
   }
 
-  if (!cached.promise) {
-    cached.promise = mongoose
-      .connect(MONGODB_URI)
-      .then((mongoose) => mongoose);
+  if (!cached!.promise) {
+    cached!.promise = mongoose.connect(MONGODB_URI);
   }
 
-  cached.conn = await cached.promise;
-  return cached.conn;
+  cached!.conn = await cached!.promise;
+  return cached!.conn;
 }
